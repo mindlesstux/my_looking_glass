64a6a7c92d87a23030cf50d5034ddffecd23be80
MindlessTux <mindlesstux@gmail.com>
2022-09-11 02:03:52 -0400

    Got the UI working.

--- api-server/main.py
+++ api-server/main.py
@@ -5,6 +5,7 @@ from fastapi.logger import logger
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 from typing import Optional, Union
 from uuid import UUID
 import datetime
@@ -162,7 +163,6 @@ async def result(
 
 @app.get(
     "/result/raw", 
-    tags=["Tests In Development"],
     summary="Fetch a raw result json",
     description="Fetches a raw result json file.",
     )
@@ -179,7 +179,6 @@ async def result_raw(
 
 @app.post(
     "/result/raw", 
-    tags=["Tests In Development"],
     summary="Fetch a raw result json",
     description="Fetches a raw result json file.",
     )
@@ -193,6 +192,59 @@ async def result_raw(
         f.close()
     
     return data
+
+# ================================================================================================================================================================
+# Function for ping tests to kick
+# ================================================================================================================================================================
+
+def kick_off_ping(uuid_str = None, dst_location=None, src_location="NopeDIEOUT", ip_family_ipv4 = False, ip_family_ipv6 = False, ping_count=10, redirect_to_null=False):
+
+    #if uuid_str == None:
+    uuid_str = str(uuid.uuid1())
+
+    ip_family = ""
+    if ip_family_ipv4 == True and ip_family_ipv6 == False:
+        ip_family = "--ipv4"
+    if ip_family_ipv4 == False and ip_family_ipv6 == True:
+        ip_family = "--ipv6"
+
+    redirect=""
+    if redirect_to_null:
+        redirect = "> /dev/null 2>&1"
+
+    print("          Trying to ping %s from %s" % (dst_location, src_location))
+    cmd = "python3 %s/lg_cmd_ping.py --uuid=%s %s --count=%s %s %s %s" % (config['BIN_PATH'], uuid_str, ip_family, ping_count, src_location, dst_location, redirect)
+    print("          CMD: %s" %(cmd))
+    new_file = "ping_%s.json" % (uuid_str);
+    write_blank_result(file_to_blank=new_file)
+    stream = os.popen(cmd)
+
+    return_str = '{"uidid": "%s", "url": "/result?fetch_uuid=%s"}' % (uuid_str, uuid_str)
+
+    return return_str
+
+# ================================================================================================================================================================
+# /run/ping-via-gui
+# ================================================================================================================================================================
+
+class TestOfPing(BaseModel):
+        dst_location: str
+        src_location: str
+        ipv4: Union[bool, None] = False
+        ipv6: Union[bool, None] = False
+        count: Union[int, None] = 10 
+
+@app.post("/run/ping-via-gui",
+    tags=["Tests In Development"],
+    summary="Run a ping test.  Body encoded, not url encoded",
+    description="Run a simple ping test.",
+    )
+async def create_item(body: TestOfPing):
+
+    # TODO: some more logic on the input and sanitization of the inputs.
+
+    return_str = kick_off_ping(dst_location=body.dst_location, src_location=body.src_location, ip_family_ipv4=body.ipv4, ip_family_ipv6=body.ipv6, ping_count=body.count)
+    return return_str
 
 # ================================================================================================================================================================
 # /run/ping
@@ -209,7 +261,7 @@ async def read_item(request: Request):
 @app.post(
     "/run/ping", 
     tags=["Tests In Development"],
-    summary="Run a ping test",
+    summary="Run a ping test.  Not body encoded, but url",
     description="Run a simple ping test.",
     )
 async def run_ping(
@@ -226,19 +278,9 @@ async def run_ping(
         ipv6: Union[bool, None] = Query(default=False, title="Force IPv6", description='Force the --ipv6 flag passed to the ping command.'),
         count: Union[int, None] = Query(default=10, ge=1, le=100, description='The number of pings to run')
     ):
-    uuid_str = str(uuid.uuid1())
-    ip_family = ""
-    if ipv4 == True and ipv6 == False:
-        ip_family = "--ipv4"
-    if ipv4 == False and ipv6 == True:
-        ip_family = "--ipv6"
 
-    print("          Trying to ping %s from %s" % (dst_location, src_location_enum.name))
-    cmd = "python3 %s/lg_cmd_ping.py --uuid=%s %s --count=%s %s %s > /dev/null 2>&1" % (config['BIN_PATH'], uuid_str, ip_family, count, src_location_enum.name, dst_location)
-    print("          CMD: %s" %(cmd))
-    stream = os.popen(cmd)
-    
-    return "{uidid: %s, url: '/result?uuidid=%s'}" % (uuid_str, uuid_str)
+    return_str = kick_off_ping(dst_location=dst_location, src_location=src_location_enum.name, ip_family_ipv4=ipv4, ip_family_ipv6=ipv6, ping_count=count)
+    return return_str
 
 # ================================================================================================================================================================
 # /run/mtr
